c0c297aa6e814055ddf231eb308e7250f8f48dae
viccyviccy <iamviccyviccy@gmail.com>
2019-09-12 16:43:13 +0300

    Updates

---
+++ precache-manifest.267f8d816f5f43b2bf0aa21757e4b17f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeff1458390eece52b0d47dc9f64303f",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "e17777bdd8f69dbe009e",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.376cff7f.chunk.css"
+  },
+  {
+    "revision": "593f2ff92af3a3fa08ee",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.7a1e98d4.chunk.js"
+  },
+  {
+    "revision": "e17777bdd8f69dbe009e",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.2127b8ac.chunk.js"
+  },
+  {
+    "revision": "249656bcd6aee9fe97fe",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.16247d91.js"
+  }
+]);
